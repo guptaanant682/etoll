d61ae2cd13dde015cfe0d791296b61c8a093dc16
Infinity <85234537+guptaanant682@users.noreply.github.com>
2023-11-03 01:03:53 +0530

    Add files via upload

---
+++ sos/script.js
@@ -0,0 +1,10 @@
+// const showAlertButton = document.getElementById('showAlert');
+// const alertContainer = document.getElementById('alertContainer');
+
+// showAlertButton.addEventListener('click', () => {
+//     alertContainer.classList.remove('hidden');
+// });
+
+// alertContainer.addEventListener('click', () => {
+//     alertContainer.classList.add('hidden');
+// });
