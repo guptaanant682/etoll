3dba59429c457ddf67becd010cb60333b25c77c6
Infinity <85234537+guptaanant682@users.noreply.github.com>
2023-11-02 22:22:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('loginButton').addEventListener('click', () => {
+    alert('Login button clicked!'); // You can replace this with actual login logic
+});
+
+
+// const fixedPointX = 380; // X coordinate
+// const fixedPointY = 330; // Y coordinate
+
+// const tl = gsap.timeline();
+
+// tl.to(".poll img", {
+//   duration: 2, // Animation duration in seconds
+//   rotation: 90, // The amount to rotate in degrees
+//   transformOrigin: `${fixedPointX}px ${fixedPointY}px`, // Set the fixed point
+// });
